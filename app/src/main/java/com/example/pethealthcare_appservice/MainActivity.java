21f9f56d80a434a1c13597799d9c95ce3ed87684
getupminaaa <getupmina0405@gmail.com>
2020-05-05 19:01:57 +0900

    user_Info 설정 추가

--- app/src/main/java/com/example/pethealthcare_appservice/MainActivity.java
+++ app/src/main/java/com/example/pethealthcare_appservice/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.pethealthcare_appservice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -14,11 +17,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user == null) {
             startMyActivity(loginActivity.class);
-        }
+        } else {
+            //회원가입 or 로그인
+                for (UserInfo profile : user.getProviderData()) {
+                    String name = profile.getDisplayName();
+                }
+            }
+
         findViewById(R.id.logoutButton).setOnClickListener(onClickListener);
     }
+
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
@@ -26,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
         android.os.Process.killProcess(android.os.Process.myPid());
         System.exit(1);
     }
+
+
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -38,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+
 
     private void startMyActivity(Class activity) {
         Intent intent = new Intent(this, activity);
---
+++ app/src/main/java/com/example/pethealthcare_appservice/memberInintActivity.java
@@ -0,0 +1,94 @@
+package com.example.pethealthcare_appservice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class memberInintActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        findViewById(R.id.loginButton).setOnClickListener(onClickListener);
+        findViewById(R.id.gotoSignUpButton).setOnClickListener(onClickListener);
+        findViewById(R.id.gotoPasswordResetButton).setOnClickListener(onClickListener);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.loginButton:
+                    login();
+                    break;
+                case R.id.gotoSignUpButton:
+                    startMyActivity(SignUpActivity.class);
+                    break;
+                case R.id.gotoPasswordResetButton:
+                    startMyActivity(passwordResetActivity.class);
+                    break;
+
+            }
+        }
+    };
+
+    private void login() {
+        String email = ((EditText) findViewById(R.id.email_editText)).getText().toString();
+        String password = ((EditText) findViewById(R.id.password_editText)).getText().toString();
+
+        if (email.length() > 0 && password.length() > 0) {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                startToast("로그인에 성공했습니다.");
+                                startMyActivity(MainActivity.class);
+                                finish();
+                            } else {
+                                if (task.getException() != null) {
+                                    startToast(task.getException().toString());
+                                }
+                            }
+                        }
+                    });
+        } else {
+            startToast("이메일 혹은 비밀번호를 입력해 주세요.");
+        }
+    }
+
+    private void startToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private void startMyActivity(Class c) {
+        Intent intent = new Intent(this, c);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+}
