7d601b98216aeccc5480c9d56740d97bb770630b
getupminaaa <getupmina0405@gmail.com>
2020-05-05 19:03:20 +0900

    Revert "user_Info 설정 추가"
    This reverts commit 21f9f56d80a434a1c13597799d9c95ce3ed87684.

--- app/src/main/java/com/example/pethealthcare_appservice/MainActivity.java
+++ app/src/main/java/com/example/pethealthcare_appservice/MainActivity.java
@@ -3,13 +3,10 @@ package com.example.pethealthcare_appservice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserInfo;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -17,21 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-        if (user == null) {
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
             startMyActivity(loginActivity.class);
-        } else {
-            //회원가입 or 로그인
-                for (UserInfo profile : user.getProviderData()) {
-                    String name = profile.getDisplayName();
-                }
-            }
-
+        }
         findViewById(R.id.logoutButton).setOnClickListener(onClickListener);
     }
-
-
     @Override
     public void onBackPressed() {
         super.onBackPressed();
@@ -39,8 +26,6 @@ public class MainActivity extends AppCompatActivity {
         android.os.Process.killProcess(android.os.Process.myPid());
         System.exit(1);
     }
-
-
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -53,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
-
 
     private void startMyActivity(Class activity) {
         Intent intent = new Intent(this, activity);
