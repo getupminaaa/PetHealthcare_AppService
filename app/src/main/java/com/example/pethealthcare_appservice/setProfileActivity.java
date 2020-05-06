511a031fb30e24417ad46e024940b462f7f33b23
getupminaaa <getupmina0405@gmail.com>
2020-05-06 16:07:50 +0900

    logic가져옴

--- app/src/main/java/com/example/pethealthcare_appservice/setProfileActivity.java
+++ app/src/main/java/com/example/pethealthcare_appservice/setProfileActivity.java
@@ -1,25 +1,52 @@
 package com.example.pethealthcare_appservice;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
 public class setProfileActivity extends AppCompatActivity {
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_user_profile);
+    }
 
-// 정보업데이트  이메일 정보 등록
-
-//      if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+//      if (FirebaseAuth
+//    }.getInstance().getCurrentUser() == null) {
 //            startMyActivity(loginActivity.class);
 //        }
 //
 //        findViewById(R.id.).setOnClickListener(onClickListener);
+// 정보업데이트  이메일 정보 등록
+
+    private void setProfile() {
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName("Jane Q. User")
+                .build();
+
+        user.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            startToast("프로필 등록 완료!");
+                        }
+                    }
+                });
     }
 
-//    View.OnClickListener onClickListener = new View.OnClickListener() {
+    //    View.OnClickListener onClickListener = new View.OnClickListener() {
 //        @Override
 //        public void onClick(View v) {
 //            switch (v.getId()) {
@@ -36,4 +63,7 @@ public class setProfileActivity extends AppCompatActivity {
 //        Intent intent = new Intent(this, activity);
 //        startActivity(intent);
 //    }
+    private void startToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
 }
