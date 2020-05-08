0513f9e0c03b9e36093763d93050d040660b776a
getupminaaa <getupmina0405@gmail.com>
2020-05-08 15:26:34 +0900

    프로필 수정 기능 추가

--- app/src/main/java/com/example/pethealthcare_appservice/updateProfileActivity.java
+++ app/src/main/java/com/example/pethealthcare_appservice/updateProfileActivity.java
@@ -10,10 +10,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -21,6 +23,12 @@ public class updateProfileActivity extends AppCompatActivity {
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+    String name;
+    String phoneNumber;
+    String address;
+    TextView tName;
+    TextView tPhoneNumber;
+    TextView tAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,17 +42,18 @@ public class updateProfileActivity extends AppCompatActivity {
                     .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                         @Override
                         public void onSuccess(DocumentSnapshot documentSnapshot) {
-                            String name = documentSnapshot.get("name").toString();
-                            String phoneNumber = documentSnapshot.get("phoneNumber").toString();
-                            String address = documentSnapshot.get("address").toString();
+                            tName = (TextView) findViewById(R.id.uUName);
+                            tPhoneNumber = (TextView) findViewById(R.id.uUPhoneNum);
+                            tAddress = (TextView) findViewById(R.id.uUAddress);
 
-                            TextView tName = (TextView) findViewById(R.id.uUName);
+                            name = documentSnapshot.get("name").toString();
+                            phoneNumber = documentSnapshot.get("phoneNumber").toString();
+                            address = documentSnapshot.get("address").toString();
+
                             tName.setText(name);
-                            TextView tPhoneNumber = (TextView) findViewById(R.id.uUPhoneNum);
                             tPhoneNumber.setText(phoneNumber);
-
-                            TextView tAddress = (TextView) findViewById(R.id.uUAddress);
                             tAddress.setText(address);
+
 //                            Log.d(TAG, "onComplete: " +documentSnapshot.get("name").toString());
                         }
                     })
@@ -66,7 +75,7 @@ public class updateProfileActivity extends AppCompatActivity {
                     if (user == null) {
                         startMyActivity(SignUpActivity.class);
                     } else {
-
+                        updateProfile();
                         break;
                     }
 
@@ -75,7 +84,30 @@ public class updateProfileActivity extends AppCompatActivity {
         }
     };
 
+    private void updateProfile() {
+        tName = (TextView) findViewById(R.id.uUName);
+        tPhoneNumber = (TextView) findViewById(R.id.uUPhoneNum);
+        tAddress = (TextView) findViewById(R.id.uUAddress);
 
+
+        DocumentReference users = db.collection("users").document(user.getUid());
+
+        users
+                .update("name",tName.getText().toString(), "phoneNumber", tPhoneNumber.getText().toString(), "address", tAddress.getText().toString())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        startToast("수정 성공");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        startToast("수정 실패");
+                    }
+                });
+
+    }
 
     private void startMyActivity(Class activity) {
         Intent intent = new Intent(this, activity);
