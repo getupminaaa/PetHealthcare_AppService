7f4ae66ac8219ad2f615800aabd9a20ce98ccab6
getupminaaa <getupmina0405@gmail.com>
2020-05-18 15:11:35 +0900

    todo 액티비티 수정

--- app/src/main/java/com/example/pethealthcare_appservice/todoList/TodoList_MainActivity.java
+++ app/src/main/java/com/example/pethealthcare_appservice/todoList/TodoList_MainActivity.java
@@ -3,35 +3,93 @@ package com.example.pethealthcare_appservice.todoList;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.pethealthcare_appservice.R;
+import com.example.pethealthcare_appservice.pet.AddPetActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
 
 public class TodoList_MainActivity extends AppCompatActivity {
+
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    ArrayList<String> petName;
+    ArrayAdapter<String> adapter;
+    ListView petName_listView;
+    EditText pPName;
+    int pos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_petcare_todo);
+        setContentView(R.layout.activity_todo_main);
 
-        findViewById(R.id.add_todo).setOnClickListener(onClickListener);
+        findViewById(R.id.add_pet).setOnClickListener(onClickListener);
+        findViewById(R.id.remove_pet).setOnClickListener(onClickListener);
+
+        petName = new ArrayList<String>();
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, petName);
+        petName_listView = (ListView) findViewById(R.id.petList);
+        petName_listView.setAdapter(adapter);
+        petName_listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        pPName = (EditText) findViewById(R.id.pPName);
+
 
     }
+
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
-                case R.id.add_todo:
-                    startMyActivity(Add_TodoActivity.class);
+                case R.id.add_pet:
+                    String text = pPName.getText().toString();
+                    if (text.length() > 0) {
+                        petName.add(text);
+                        pPName.setText("");
+                        adapter.notifyDataSetChanged();
+                        startMyActivity(AddPetActivity.class);
+                    } else {
+                        startToast("내용을 입력해주세요!");
+                    }
+                    break;
+
+                case R.id.remove_pet:
+                    pos = petName_listView.getCheckedItemPosition();
+                    if (pos != ListView.INVALID_POSITION) {
+                        petName.remove(pos);
+                        petName_listView.clearChoices();
+                        adapter.notifyDataSetChanged();
+//                        remove_petDocument();
+                    } else {
+                        startToast("선택해주세요!");
+                    }
                     break;
             }
         }
     };
 
+
+
     private void startMyActivity(Class activity) {
         Intent intent = new Intent(this, activity);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
+    }
+
+    private void startToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
 
